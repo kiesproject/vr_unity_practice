086efcd787a7aea4c0aec8733d8a422540e08f0a
eijiMiyazawa <b1800722@planet.kanazawa-it.ac.jp>
2019-11-01 19:21:46 +0900

    ダンジョン生成を実験

---
+++ pra2019_11_project/Assets/script/DangeonGenerator.cs
@@ -0,0 +1,235 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangeonGenerator : MonoBehaviour
+{
+    //ステージの高さと幅を設定
+    [SerializeField] int MapHeight = 50;
+    [SerializeField] int MapWidth = 50;
+
+    public int[,] Map;  //マップ構成データ
+
+    // 識別のための定数
+    const int wall = 9;
+    const int road = 0;
+
+    // 生成される壁のゲームオブジェクト
+    public GameObject WallObject;
+    
+    // 部屋の数や大きさを指定
+    [SerializeField] int roomMinHeight = 5;
+    [SerializeField] int roomMaxHeight = 10;
+
+    [SerializeField] int roomMinWidth = 5;
+    [SerializeField] int roomMaxWidth = 10;
+
+    [SerializeField] int RoomCountMin = 10;
+    [SerializeField] int RoomCountMax = 15;
+
+    // 道の集合点の数
+    [SerializeField] int meetPointCount = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetMapData();
+
+        CreateSpeceData();
+
+        CreateStage();
+    }
+
+    /// <summary>
+    /// Mapデータ（二次元配列）の初期化
+    /// </summary>
+    private void ResetMapData()
+    {
+        Map = new int[MapHeight, MapWidth];
+        for (int i = 0; i < MapHeight; i++)
+        {
+            for(int j = 0; j < MapWidth; j++)
+            {
+                Map[i, j] = wall;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 空白部分のデータを変更
+    /// </summary>
+    private void CreateSpeceData()
+    {
+        int roomCount = Random.Range(RoomCountMin, RoomCountMax);
+
+        int[] meetPointsX = new int[meetPointCount];
+        int[] meetPointsY = new int[meetPointCount];
+        for (int i = 0; i < meetPointsX.Length; i++)
+        {
+            meetPointsX[i] = Random.Range(MapWidth / 4, MapWidth * 3 / 4);
+            meetPointsY[i] = Random.Range(MapHeight / 4, MapHeight * 3 / 4);
+            Map[meetPointsY[i], meetPointsX[i]] = road;
+        }
+
+        for (int i = 0; i < roomCount; i++)
+        {
+            int roomHeight = Random.Range(roomMinHeight, roomMaxHeight);
+            int roomWidth = Random.Range(roomMinWidth, roomMaxWidth);
+            int roomPointX = Random.Range(2, MapWidth - roomMaxWidth - 2);
+            int roomPointY = Random.Range(2, MapWidth - roomMaxWidth - 2);
+
+            int roadStartPointX = Random.Range(roomPointX, roomPointX + roomWidth);
+            int roadStartPointY = Random.Range(roomPointY, roomPointY + roomHeight);
+
+            bool isRoad = CreateRoomData(roomHeight, roomWidth, roomPointX, roomPointY);
+
+            if (isRoad == false)
+            {
+                CreateRoadData(roadStartPointX, roadStartPointY, meetPointsX[Random.Range(0, 0)], meetPointsY[Random.Range(0, 0)]);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 部屋データを生成。すでに部屋がある場合はtrueを返し、道を作らないようにする
+    /// </summary>
+    /// <param name="roomHeight">部屋の高さ</param>
+    /// <param name="roomWidth">部屋の横幅</param>
+    /// <param name="roomPointX">部屋の始点(x)</param>
+    /// <param name="roomPointY">部屋の始点(y)</param>
+    /// <returns></returns>
+    private bool CreateRoomData(int roomHeight, int roomWidth, int roomPointX, int roomPointY)
+    {
+        bool beCreateRoad = false;
+        for (int i = 0; i < roomHeight; i++)
+        {
+            for (int j = 0; j < roomWidth; j++)
+            {
+                if (Map[roomPointY + i, roomPointX + j] == road)
+                {
+                    beCreateRoad = true;
+                }
+                else
+                {
+                    Map[roomPointY + i, roomPointX + j] = road;
+                }
+            }
+        }
+        return beCreateRoad;
+    }
+
+    /// <summary>
+    /// 道データを生成
+    /// </summary>
+    /// <param name="roadStartPointX"></param>
+    /// <param name="roadStartPointY"></param>
+    /// <param name="meetPointX"></param>
+    /// <param name="meetPointY"></param>
+    private void CreateRoadData(int roadStartPointX, int roadStartPointY, int meetPointX, int meetPointY)
+    {
+
+        bool isRight;
+        if (roadStartPointX > meetPointX)
+        {
+            isRight = true;
+        }
+        else
+        {
+            isRight = false;
+        }
+        bool isUnder;
+        if (roadStartPointY > meetPointY)
+        {
+            isUnder = false;
+        }
+        else
+        {
+            isUnder = true;
+        }
+
+        if (Random.Range(0, 2) == 0)
+        {
+
+            while (roadStartPointX != meetPointX)
+            {
+
+                Map[roadStartPointY, roadStartPointX] = road;
+                if (isRight == true)
+                {
+                    roadStartPointX--;
+                }
+                else
+                {
+                    roadStartPointX++;
+                }
+
+            }
+
+            while (roadStartPointY != meetPointY)
+            {
+
+                Map[roadStartPointY, roadStartPointX] = road;
+                if (isUnder == true)
+                {
+                    roadStartPointY++;
+                }
+                else
+                {
+                    roadStartPointY--;
+                }
+            }
+
+        }
+        else
+        {
+
+            while (roadStartPointY != meetPointY)
+            {
+
+                Map[roadStartPointY, roadStartPointX] = road;
+                if (isUnder == true)
+                {
+                    roadStartPointY++;
+                }
+                else
+                {
+                    roadStartPointY--;
+                }
+            }
+
+            while (roadStartPointX != meetPointX)
+            {
+
+                Map[roadStartPointY, roadStartPointX] = road;
+                if (isRight == true)
+                {
+                    roadStartPointX--;
+                }
+                else
+                {
+                    roadStartPointX++;
+                }
+
+            }
+
+        }
+    }
+
+
+    /// <summary>
+    /// マップデータをもとにオブジェクトを生成
+    /// </summary>
+    private void CreateStage()
+    {
+        for (int i = 0; i < MapHeight; i++)
+        {
+            for (int j = 0; j < MapWidth; j++)
+            {
+                if (Map[i, j] == wall)
+                {
+                    Instantiate(WallObject, new Vector3(j - MapWidth / 2, i - MapHeight / 2, 0), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
