16324ad33a8654105caf1c064a7d27594ed3ca65
eiji_miyazawa <b1800722@planet.kanazawa-it.ac.jp>
2019-11-03 22:12:20 +0900

    ProBuilder導入で仮のチャンクを作成＆生成プログラム作成

---
+++ pra2019_11_project/Assets/Pack/Script/FPSCam.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCam : MonoBehaviour {
+
+    public float mouseSpeed = 1;
+    public GameObject Haed;
+    CharacterController characterController;
+
+    public float MoveSpeed = 10.0f;
+    public float JumpSpeed = 20.0f;
+    public float Gravity = 9.8f;
+
+    Vector3 MoveVector = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+   
+        characterController = GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+
+        this.transform.Rotate(Vector3.up, x * mouseSpeed * Time.deltaTime,Space.World);
+        Haed.transform.Rotate(this.transform.right, -y * mouseSpeed * Time.deltaTime, Space.World);
+
+        float Move_x = Input.GetAxis("Horizontal");
+        float Move_z = Input.GetAxis("Vertical");
+
+
+        if (characterController.isGrounded) {
+            if (Move_x != 0)
+            {
+
+                MoveVector.x = Move_x * MoveSpeed;
+
+            }
+            else
+            {
+
+                MoveVector.x = 0;
+
+            }
+
+            if (Move_z != 0)
+            {
+
+                MoveVector.z = Move_z * MoveSpeed;
+
+            }
+            else
+            {
+
+                MoveVector.z = 0;
+
+            }
+
+            if (Input.GetButtonDown("Jump")) {
+                MoveVector.y = JumpSpeed;
+            }
+
+        }
+
+        MoveVector.y -= Gravity * Time.deltaTime;
+
+        characterController.Move( transform.TransformDirection(MoveVector * Time.deltaTime) );
+
+    }
+}
--- pra2019_11_project/Assets/script/ChunkGenerator.cs
+++ pra2019_11_project/Assets/script/ChunkGenerator.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class ChunkGenerator : MonoBehaviour
 {
+    [SerializeField] GameObject[] Chunks;
+
+    public int[,] mapData;
+
+    [SerializeField] int MapX = 3, MapZ = 3;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        ResetMapData();
+
+        CreateStageData();
+
+        CreateStage();
+
+        for (int i = 0; i < MapX; i++)
+        {
+            for (int j = 0; j < MapZ; j++)
+            {
+                Debug.Log("[" + i + "," + j + "]" + mapData[i,j]);
+            }
+        }
+    }
+    
+    private void ResetMapData()
+    {
+        mapData = new int[MapX, MapZ];
+        for(int i = 0; i < MapX; i++)
+        {
+            for(int j = 0; j < MapZ; j++)
+            {
+                mapData[i, j] = 0;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateStageData()
     {
-        
+        for (int i = 0; i < MapX; i++)
+        {
+            for (int j = 0; j < MapZ; j++)
+            {
+                int selectedChunk = Random.Range(0, Chunks.Length);
+                Debug.Log(selectedChunk);
+                mapData[i, j] = selectedChunk;
+            }
+        }
+    }
+
+    private void CreateStage()
+    {
+        for (int i = 0; i < MapX; i++)
+        {
+            for (int j = 0; j < MapZ; j++)
+            {
+                Instantiate(Chunks[mapData[i, j]], new Vector3(10 * i, 0, 10 * j), Quaternion.identity);
+            }
+        }
     }
 }
