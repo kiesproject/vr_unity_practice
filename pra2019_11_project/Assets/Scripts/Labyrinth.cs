d93b4c101a6d9e6d19c4733a8485076e6009d433
Unknown <b1704736@planet.kanazawa-it.ac.jp>
2019-11-11 03:43:32 +0900

    隣接矩形の探索を実装

--- pra2019_11_project/Assets/Scripts/Labyrinth.cs
+++ pra2019_11_project/Assets/Scripts/Labyrinth.cs
@@ -15,9 +15,18 @@ public class Labyrinth : MonoBehaviour
         rect.Create_ChildRect(1);
         rect.rects[0].Create_ChildRect(1);
         rect.rects[1].Create_ChildRect(0);
-        //Debug.Log(rect.rects[1].);
+        rect.rects[1].rects[1].Create_ChildRect(1);
         Test_RectTree(rect, 0);
 
+
+        var v = Rect.Check_pointUpRect(1, 3, rect);
+        Rect.Test_Rect(v);
+
+        Debug.Log("--- --- --- --- --- --- --- ---");
+        foreach(Rect r in v.Get_AdjaRect(rect))
+        {
+            Rect.Test_Rect(r);
+        }
     }
 
     /// <summary>
@@ -27,6 +36,9 @@ public class Labyrinth : MonoBehaviour
     {
         
     }
+
+    
+
 
     /// <summary>
     /// Rectの階層を表示する
@@ -47,7 +59,7 @@ public class Labyrinth : MonoBehaviour
             ts += "----";
 
         //文字列出力
-        ts += string.Format("〇 x: {0} y: {1} width: {2} height: {3}", rect.x, rect.y, rect.width, rect.height); ;
+        ts += string.Format("〇 x: {0} y: {1} width: {2} height: {3}", rect.x, rect.y, rect.width, rect.height);
         Debug.Log(ts);
 
         //再帰的に実行する
@@ -58,6 +70,8 @@ public class Labyrinth : MonoBehaviour
         }
 
     }
+
+   
 
 }
 
@@ -122,7 +136,18 @@ sealed public class Rect
     /// <returns></returns>
     public bool Check_ChildRect(int _x, int _y, int _width, int _height)
     {
-        return ((_x >= 0) && (_y >= 0) && (_width <= x + width) && (_height <= y + height)) ? true : false;
+        return ((_x >= 0) && (_y >= 0) && (_width <= width) && (_height <= height)) ? true : false;
+    }
+
+    /// <summary>
+    /// 指定した点が自分の中に納まっているかの判定
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
+    public bool Check_pointRect(int x, int y)
+    {
+        return ((this.x <= x) && (this.y <= y) && (x <= this.x + width - 1) && (y <= this.y + height - 1));
     }
 
     /// <summary>
@@ -148,26 +173,35 @@ sealed public class Rect
     }
 
 
+    /// <summary>
+    /// 矩形の中に隣接した二個の矩形を生成する
+    /// </summary>
+    /// <param name="length"></param>
+    /// <param name="dir"></param>
     public void Create_ChildRect(int length, int dir)
     {
         if(dir == 0)
         {
             //縦に区切るときの処理
             Create_ChildRect(x, y, length, height);
-            Create_ChildRect(length, y, width - length, height);
+            Create_ChildRect(x + length, y, width - length, height);
         }
         else if(dir == 1)
         {
             //横に区切るときの処理
             Create_ChildRect(x, y, width, length);
-            Create_ChildRect(x, length, width, height - length);
+            Create_ChildRect(x, y + length, width, height - length);
         }
         else
         {
             Debug.LogWarning("[Rect]生成失敗");
         }
     }
-    
+
+    /// <summary>
+    /// 矩形の中に隣接した二個の同じ大きさの矩形を生成する
+    /// </summary>
+    /// <param name="dir"></param>
     public void Create_ChildRect(int dir)
     {
         int l = 0;
@@ -188,5 +222,120 @@ sealed public class Rect
         Create_ChildRect(l, dir);
     }
 
+    
+    /// <summary>
+    /// 指定した座標の一番上に来る矩形を取得する。
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="origin"></param>
+    /// <returns></returns>
+    public static Rect Check_pointUpRect(int x, int y, Rect origin)
+    {
+        Rect rect = origin;
+        bool isBreak = false;
+
+        while (true)
+        {
+            foreach (Rect r in rect.rects)
+            {
+                if (r.Check_pointRect(x, y))
+                {
+                    rect = r;
+                    isBreak = true;
+                }
+            }
+
+            if (isBreak)
+            {
+                isBreak = false;
+                continue;
+            }
+            break;
+        }
+        return rect;
+    }
+
+    /// <summary>
+    /// 隣接した矩形を探索する
+    /// </summary>
+    /// <param name="origin"></param>
+    /// <returns></returns>
+    public List<Rect> Get_AdjaRect(Rect origin)
+    {
+        //方向設定
+        Vector3Int[] dir = { new Vector3Int(-1, 0, 0), new Vector3Int(0, 1, 0), new Vector3Int(1, 0, 0), new Vector3Int(0, -1, 0) };
+
+        //方向を決定する要素
+        int index = 0;
+
+        //外周をなぞるターゲット
+        Vector3Int terget = new Vector3Int(x, y, 0);
+
+        //出力するRectリスト
+        List<Rect> list = new List<Rect>();
+        
+        //終了フラグ
+        bool isEnd = false;
+
+        int j=0; //安全用
+        while (!isEnd)
+        {
+            if (j++ > 1000) break; //安全用
+
+            Debug.Log(terget);
+
+            //ターゲットの地点が範囲外なら次に進む
+            if (!origin.Check_pointRect(terget.x + dir[index % 4].x, terget.y + dir[index % 4].y))
+            {
+                next();
+                continue;
+            }
+
+            //指定した地点を調べる
+            var rect = Check_pointUpRect(terget.x + dir[index % 4].x, terget.y + dir[index % 4].y , origin);
+
+            //隣接したRectを記録する
+            if (!list.Contains(rect))
+            {
+                list.Add(rect);
+                Rect.Test_Rect(rect);
+            }
+
+            next();
+        }
+
+        return list;
+
+        //ターゲットを進める
+        void next()
+        {
+            //ターゲットの進行方向が矩形外
+            if (!Check_pointRect(terget.x + dir[(index + 1) % 4].x, terget.y + dir[(index + 1) % 4].y))
+            {
+                //方向を変える
+                index++;
+                //ターゲットを進ませる
+                terget += dir[(index + 1) % 4];
+            }
+            else
+            {
+                //ターゲットを進ませる
+                terget += dir[(index + 1) % 4];
+            }
+
+            //元の場所に戻ってきたら終了する
+            if (terget == new Vector3Int(x, y, 0))
+            {
+                isEnd = true;
+            }
+        }
+
+    }
+
+    public static void Test_Rect(Rect rect)
+    {
+        Debug.Log(string.Format("[Test] x: {0} y: {1} width: {2} height: {3}", rect.x, rect.y, rect.width, rect.height));
+    }
 }
 
