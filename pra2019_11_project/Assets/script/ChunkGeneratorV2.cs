e89de37a2de51b90389dfe13d1980c958224dfd9
eiji_miyazawa <b1800722@planet.kanazawa-it.ac.jp>
2019-11-25 23:22:51 +0900

    外壁を作るプログラムまで完成

--- pra2019_11_project/Assets/script/ChunkGeneratorV2.cs
+++ pra2019_11_project/Assets/script/ChunkGeneratorV2.cs
@@ -32,6 +32,8 @@ public class ChunkGeneratorV2 : MonoBehaviour
         CreateStageData();
 
         CreateStage();
+
+        DebugData();
     }
 
     private void ResetMapData()
@@ -51,7 +53,31 @@ public class ChunkGeneratorV2 : MonoBehaviour
 
     private void CreateStageData()
     {
-        
+        //　壁の判定のため、外側は通路に
+        for (int i = 0; i < MapX; i++)
+        {
+            for (int j = 0; j < MapZ; j++)
+            {
+                if(i == 0 || j == 0 || i == MapX - 1 || j == MapZ - 1)
+                {
+                    mapData[i, j].ChunkIndex = 1;
+                }
+                else
+                {
+                    mapData[i, j].ChunkIndex = 0;
+                }
+            }
+        }
+
+        Dig(1, 1);
+
+    }
+
+    private void Dig(int x, int z)
+    {
+        Random rand = new Random();
+
+
     }
 
     private void CreateStage()
@@ -63,5 +89,20 @@ public class ChunkGeneratorV2 : MonoBehaviour
                 Instantiate(Chunks[mapData[i, j].ChunkIndex], new Vector3(10 * i, 0, 10 * j), Quaternion.identity);
             }
         }
+    }
+
+    private void DebugData()
+    {
+        string debugData = "";
+        for (int i = 0; i < MapX; i++)
+        {
+            for (int j = 0; j < MapZ; j++)
+            {
+                debugData += mapData[i, j].ChunkIndex;
+            }
+            debugData += "\n";
+        }
+
+        Debug.Log(debugData);
     }
 }
