7c49e2926645a4e2768dd42c3b6c7db6e45d4372
yoshi04141 <b1915215@planet.kanazawa-it.ac.jp>
2019-11-26 17:50:47 +0900

    ステージの作成

---
+++ pra2019_11_project/Assets/ClearScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color color = gameObject.GetComponent<Renderer>().material.color;
+        color.a = 0.9f;
+
+        gameObject.GetComponent<Renderer>().material.color = color;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ pra2019_11_project/Assets/RotateCube.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+
+    public float rotateSpeed = 1.0f;          // 回転速度
+    public float rotateDuration = 1.0f;       // 回転の継続時間
+    public float rotateDirection = 0;         // 回転方向　0…左　1…右　消す可能性アリ
+
+    float rotateElapsedTime = 0;              // 回転経過時間
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(new Vector3(0, 0, 5));
+
+        // transform.Rotate(new Vector3(0, 0, 5));
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            transform.Rotate(new Vector3(0, 0, 5));
+        }
+
+    }
+}
