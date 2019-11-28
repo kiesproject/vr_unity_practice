39170e30a1babfcd1e5b910299c08a51940c98ef
eiji_miyazawa <b1800722@planet.kanazawa-it.ac.jp>
2019-11-29 07:10:32 +0900

    動的ナビゲーションのAPI導入

---
+++ pra2019_11_project/Assets/script/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GameController : MonoBehaviour
+{
+    private enum State
+    {
+        Dig,
+        Ready,
+        Play,
+        End
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
