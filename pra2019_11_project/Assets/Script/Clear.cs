8a4d96a0315cd66d768831dbbe0587ac6bdd0a2e
miyakawamizuki <b1901780@planet.kanazawa-it.ac.jp>
2019-11-29 22:04:54 +0900

    11月の課題の提出です。

---
+++ pra2019_11_project/Assets/Script/Clear.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clear : MonoBehaviour
+{
+    public GameObject Text;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Text.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // オブジェクトに触れたらText表示する
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Text.SetActive(true);
+        }
+    }
+}
---
+++ pra2019_11_project/Assets/Script/GearRotate2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearRotate2 : MonoBehaviour
+{
+    void Update()
+    {
+        //z軸にー3回す
+        transform.Rotate(new Vector3(0, 0, -3));
+    }
+}
