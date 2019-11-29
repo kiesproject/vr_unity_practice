73a2eb1209dd567319e22f9990c4d9d9df820ac7
takada <b1915905@planet.kanazawa-it.ac.jp>
2019-11-30 05:46:27 +0900

    課題提出
    unityの玉転がしのしりょうをおうようしてつくりました。GAMEOVERの画面がうまくシーン遷移できませんでした。

---
+++ pra2019_11_project/Assets/Scenes/gameover.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class atarihantei : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Gameover")
+        {
+            Application.LoadLevel("Gameover");
+        }
+    }
+}
