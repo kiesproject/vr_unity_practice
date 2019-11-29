785e9af856c2e31a41298f8d2cfee39a19204898
yoshi04141 <b1915215@planet.kanazawa-it.ac.jp>
2019-11-29 16:35:20 +0900

    細かい修正

---
+++ pra2019_11_project/Assets/GoalScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalScript : MonoBehaviour
+{
+    // ゴール時に表示するText
+    public GameObject goalText;
+
+    // トリガーに接触した瞬間に呼ばれる処理
+    void OnTriggerEnter(Collider collider)
+    {
+        // 接触したのがプレイヤーか？
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            // ゴールを表示
+            goalText.SetActive(true);
+        }
+    }
+}
