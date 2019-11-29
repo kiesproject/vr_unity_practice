305981832afe389bc664d9736ea354951d3acb94
kaito1944 <b1901735@planet.kanazawa-it.ac.jp>
2019-11-29 19:29:12 +0900

    課題提出

---
+++ pra2019_11_project/Assets/Materials/scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour{
+    public GameObject target;
+    UnityEngine.AI.NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start() 
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.transform.position;
+    }
+}
