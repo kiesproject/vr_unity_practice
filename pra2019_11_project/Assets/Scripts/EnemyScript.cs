1e916390acbf2df25af3c5160b120768ade8d0a6
Doragongon <b1916010@planet.kanazawa-it.ac.jp>
2019-11-29 20:05:40 +0900

    課題提出

---
+++ pra2019_11_project/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript: MonoBehaviour
+{
+
+    public NavMeshAgent Enemy;
+    public GameObject Player;
+
+    void Start()
+    {
+        Enemy = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Player != null)
+        {
+            Enemy.destination = Player.transform.position;
+        }
+    }
+}
