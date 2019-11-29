cf22770240f0bae4be829ce6a8a7d2a25d22a534
yuki <b1916294@planet.kanazawa-it.ac.jp>
2019-11-29 19:00:39 +0900

    課題

---
+++ pra2019_11_project/Assets/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Enemy : MonoBehaviour
+{
+
+    GameObject Player;
+    GameObject homingObj;
+    public float Speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        Player = GameObject.Find("nige");
+        homingObj = GameObject.Find("oni");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(Player.transform.position.x, Player.transform.position.y, Player.transform.position.z), 9.0f * Time.deltaTime);
+    }
+}
---
+++ pra2019_11_project/Assets/nige.cs
@@ -0,0 +1,46 @@
+﻿// Player.cs
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// プレイヤー
+public class nige : MonoBehaviour
+{
+
+    void Update()
+    {
+        // 左に移動
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(-0.2f, 0.0f, 0.0f);
+        }
+        // 右に移動
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(0.2f, 0.0f, 0.0f);
+        }
+        // 前に移動
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(0.0f, 0.0f, 0.2f);
+        }
+        // 後ろに移動
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(0.0f, 0.0f, -0.2f);
+        }
+        
+    }
+    void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.name == "oni")
+            {
+                SceneManager.LoadScene("GAME OVER");
+            }
+            if (collision.gameObject.name == "Goal")
+            {
+                SceneManager.LoadScene("Clear");
+            }
+    }
+}
